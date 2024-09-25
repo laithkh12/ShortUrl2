3c9446c48f05d38738fa6be497aa4ceac76a097b
laithkh12 <168769648+laithkh12@users.noreply.github.com>
2024-09-25 15:06:35 +0300

    Update shortUrl.js

--- models/shortUrl.js
+++ models/shortUrl.js
@@ -1,23 +1,55 @@
 const mongoose = require('mongoose');
-const shortId = require('shortid')
+const shortId = require('shortid');
 
+/**
+ * @typedef ShortUrl
+ * @type {Object}
+ * @property {string} full - The original full URL that needs to be shortened.
+ * @property {string} short - The generated short URL, which is unique for each entry.
+ * @property {number} clicks - The number of times the short URL has been clicked.
+ */
 
-
+/**
+ * Schema for storing short URLs in the database.
+ * @type {mongoose.Schema}
+ */
 const shortUrlSchema = new mongoose.Schema({
-    full:{
-        type:String,
-        required:true, 
+    /**
+     * The original full URL that needs to be shortened.
+     * @type {String}
+     * @required
+     */
+    full: {
+        type: String,
+        required: true,
     },
-    short:{
-        type:String,
-        required:true,
-        default:shortId.generate
+    /**
+     * The generated short URL.
+     * This field is automatically generated using the shortId library.
+     * @type {String}
+     * @required
+     * @default shortId.generate
+     */
+    short: {
+        type: String,
+        required: true,
+        default: shortId.generate
     },
-    clicks:{
-        type:Number,
-        required:true,
-        default:0
+    /**
+     * The number of times the short URL has been clicked.
+     * @type {Number}
+     * @required
+     * @default 0
+     */
+    clicks: {
+        type: Number,
+        required: true,
+        default: 0
     }
-})
+});
 
-module.exports = mongoose.model('ShortUrl', shortUrlSchema)
+/**
+ * Model for the ShortUrl schema.
+ * @type {mongoose.Model<ShortUrl>}
+ */
+module.exports = mongoose.model('ShortUrl', shortUrlSchema);
